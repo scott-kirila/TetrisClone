a0bbcec22e564152f65cc1150e0f3e77b903fa27
Scott Kirila <68308305+scott-kirila@users.noreply.github.com>
2025-04-07 23:19:45 +0900

    Rewrote movement
    Check is now made with movement input

--- Source/TetrisClone/Piece.cpp
+++ Source/TetrisClone/Piece.cpp
@@ -19,34 +19,26 @@ APiece::APiece()
 	// Root Block
 	Block0 = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("RootBlock"));
 	Block0->SetupAttachment(RootComponent);
-
-	Collider0 = CreateDefaultSubobject<USphereComponent>(TEXT("RootColliderA"));
-	Collider0->SetupAttachment(Block0);
-	Collider0->SetSphereRadius(70.0f);
+	Block0->SetCollisionResponseToChannel(ECC_Visibility, ECR_Block);
+	Block0->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
 
 	// Block 1
 	Block1 = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Block1"));
 	Block1->SetupAttachment(RootComponent);
-
-	Collider1 = CreateDefaultSubobject<USphereComponent>(TEXT("Collider1A"));
-	Collider1->SetupAttachment(Block1);
-	Collider1->SetSphereRadius(70.0f);
+	Block1->SetCollisionResponseToChannel(ECC_Visibility, ECR_Block);
+	Block1->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
 	
 	// Block 2
 	Block2 = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Block2"));
 	Block2->SetupAttachment(RootComponent);
-
-	Collider2 = CreateDefaultSubobject<USphereComponent>(TEXT("Collider2A"));
-	Collider2->SetupAttachment(Block2);
-	Collider2->SetSphereRadius(70.0f);
+	Block2->SetCollisionResponseToChannel(ECC_Visibility, ECR_Block);
+	Block2->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
 
 	// Block 3
 	Block3 = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Block3"));
 	Block3->SetupAttachment(RootComponent);
-
-	Collider3 = CreateDefaultSubobject<USphereComponent>(TEXT("Collider3A"));
-	Collider3->SetupAttachment(Block3);
-	Collider3->SetSphereRadius(70.0f);
+	Block3->SetCollisionResponseToChannel(ECC_Visibility, ECR_Block);
+	Block3->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
 
 	Blocks = { Block0, Block1, Block2, Block3 };
 }
@@ -70,18 +62,6 @@ void APiece::BeginPlay()
 		PlayerController->Possess(this);
 	}
 
-	Collider0->OnComponentBeginOverlap.AddDynamic(this, &APiece::OnBeginOverlap);
-	Collider0->OnComponentEndOverlap.AddDynamic(this, &APiece::OnEndOverlap);
-
-	Collider1->OnComponentBeginOverlap.AddDynamic(this, &APiece::OnBeginOverlap);
-	Collider1->OnComponentEndOverlap.AddDynamic(this, &APiece::OnEndOverlap);
-
-	Collider2->OnComponentBeginOverlap.AddDynamic(this, &APiece::OnBeginOverlap);
-	Collider2->OnComponentEndOverlap.AddDynamic(this, &APiece::OnEndOverlap);
-
-	Collider3->OnComponentBeginOverlap.AddDynamic(this, &APiece::OnBeginOverlap);
-	Collider3->OnComponentEndOverlap.AddDynamic(this, &APiece::OnEndOverlap);
-
 	GetWorldTimerManager().SetTimer(DropTimer, this, &APiece::OnDropTimeout, 1.0f, true, 1.0f);
 
 }
@@ -90,6 +70,12 @@ void APiece::BeginPlay()
 void APiece::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+
+	FVector Direction = { 0.0f, 0.0f, -1.0f };
+	if (!CanMoveToward(Direction))
+	{
+		bCanMoveDown = false;
+	}
 	
 	if (!bCanMoveDown)
 	{
@@ -120,19 +106,25 @@ void APiece::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 void APiece::MoveHorizontally(const FInputActionValue& Value)
 {
 	float ActionValue = Value.Get<float>();
-	float Direction = FMath::Sign(ActionValue);
+	float Sign = FMath::Sign(ActionValue);
+
+	FVector DirectionVector = { ActionValue, 0.0f, 0.0f };
+	if (!CanMoveToward(DirectionVector)) return;
 	
 	auto CurrentLocation = GetActorLocation();
 
-	if ((bCanMoveLeft && Direction < 0.0f) || (bCanMoveRight && Direction > 0.0f))
+	if ((Sign < 0.0f) || (Sign > 0.0f))
 	{
-		SetActorLocation({CurrentLocation.X + Direction * 100.0f, CurrentLocation.Y, CurrentLocation.Z});
+		SetActorLocation({CurrentLocation.X + Sign * 100.0f, CurrentLocation.Y, CurrentLocation.Z});
 	}
 }
 
 void APiece::DownwardBurst()
 {
 	if (!bCanMoveDown) return;
+
+	FVector Direction = { 0.0f, 0.0f, -1.0f };
+	if (!CanMoveToward(Direction)) return;
 	
 	auto CurrentLocation = GetActorLocation();
 	SetActorLocation({CurrentLocation.X, CurrentLocation.Y, CurrentLocation.Z - 100.0f});
@@ -183,6 +175,9 @@ void APiece::OnDropTimeout()
 {
 	if (!bCanMoveDown) return;
 	
+	FVector Direction = { 0.0f, 0.0f, -1.0f};
+	if (!CanMoveToward(Direction)) return;
+	
 	auto CurrentLocation = GetActorLocation();
 	
 	auto NewLocation = FVector(CurrentLocation.X, CurrentLocation.Y, CurrentLocation.Z - 100.0f);
@@ -201,14 +196,7 @@ void APiece::OnSpawnTimeout()
 
 void APiece::Stop()
 {
-	bCanMoveLeft  = false;
-	bCanMoveRight = false;
 	bCanRotate    = false;
-
-	Collider0->SetCollisionResponseToAllChannels(ECR_Block);
-	Collider1->SetCollisionResponseToAllChannels(ECR_Block);
-	Collider2->SetCollisionResponseToAllChannels(ECR_Block);
-	Collider3->SetCollisionResponseToAllChannels(ECR_Block);
 	
 	GetWorldTimerManager().ClearTimer(DropTimer);
 }
@@ -221,63 +209,28 @@ void APiece::SpawnNewPiece()
 	GetWorldTimerManager().SetTimer(SpawnTimer, this, &APiece::OnSpawnTimeout, 1.0f, false, 1.0f);
 }
 
-void APiece::OnBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
-                            UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+bool APiece::CanMoveToward(FVector Direction)
 {
-	if (OtherActor == this) return;
+	bool Result = true;
 
-	FVector Down = -FVector::UpVector;
-	FVector Left = -FVector::ForwardVector;
-	FVector Right = -Left;
-	TArray<FVector> Directions = { Down, Left, Right };
-
-	for (const auto& Direction : Directions)
-	{
-		FHitResult HitResult;
-		FVector Start = OverlappedComponent->GetComponentLocation();
-		FVector End = Start + Direction * 100.0f;
-
-		FCollisionQueryParams QueryParameters;
-		QueryParameters.AddIgnoredActor(this);
-
-		DrawDebugLine(GetWorld(), Start, End, FColor::Red, true, 1, 0, 5);
-		auto bDidHit = GetWorld()->LineTraceSingleByChannel(HitResult, Start, End, ECC_Visibility, QueryParameters);
-		
-		bCanMoveDown  &= (Direction != Down)  || !bDidHit;
-		bCanMoveLeft  &= (Direction != Left)  || !bDidHit;
-		bCanMoveRight &= (Direction != Right) || !bDidHit;
-	}
-}
-
-void APiece::OnEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp,
-	int32 OtherBodyIndex)
-{
-	if (OtherActor == this) return;
-
-	// Changing the actual values early could introduce movement bugs!
-	bool _bCanMoveLeft  = true;
-	bool _bCanMoveRight = true;
-
-	FVector Left = -FVector::ForwardVector;
-	FVector Right = -Left;
-	TArray<FVector> Directions = { Left, Right };
+	TArray<UStaticMeshComponent*> Meshes;
+	GetComponents<UStaticMeshComponent>(Meshes);
 	
-	for (const auto& Direction : Directions)
+	for (const auto& Mesh : Meshes)
 	{
 		FHitResult HitResult;
-		FVector Start = OverlappedComponent->GetComponentLocation();
+		FVector Start = Mesh->GetComponentLocation();
 		FVector End = Start + Direction * 100.0f;
 
-		FCollisionQueryParams QueryParameters;
-		QueryParameters.AddIgnoredActor(this);
-		
-		DrawDebugLine(GetWorld(), Start, End, FColor::Green, true, 1, 0, 5);
-		auto bDidHit = GetWorld()->LineTraceSingleByChannel(HitResult, Start, End, ECC_Visibility, QueryParameters);
+		FCollisionQueryParams QueryParams;
+		QueryParams.AddIgnoredActor(this);
 
-		_bCanMoveLeft  &= (Direction != Left)  || !bDidHit;
-		_bCanMoveRight &= (Direction != Right) || !bDidHit;
+		auto bDidHit = GetWorld()->LineTraceSingleByChannel(HitResult, Start, End, ECC_Visibility, QueryParams);
+		auto Color = bDidHit ? FColor::Red : FColor::Green;
+		DrawDebugLine(GetWorld(), Start, End, Color, false, 1, 0, 5);
+
+		Result &= !bDidHit;
 	}
 
-	bCanMoveLeft  = _bCanMoveLeft;
-	bCanMoveRight = _bCanMoveRight;
+	return Result;
 }
